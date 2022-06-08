928a6b87e977ff877e597d49f1222ed9095515b0
Dyson-Wang <dyson.wang@outlook.com>
2022-06-08 17:19:53 +0800

    GF

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -1,10 +1,13 @@
 <template>
     <form @submit.prevent="onSubmit">
-        <label for="new-todo-input">
-            What needs to be done?
-        </label>
-        <input type="text" id="new-todo-input" name="new-todo" autocomplete="off" v-model.lazy.trim="label">
-        <button type="submit">Add</button>
+        <h2 class="label-wrapper">
+            <label for="new-todo-input">
+                What needs to be done?
+            </label>
+        </h2>
+
+        <input type="text" id="new-todo-input" name="new-todo" autocomplete="off" v-model.lazy.trim="label" class="input__lg">
+        <button type="submit" class="btn btn__primary btn__lg">Add</button>
     </form>
 </template>
 
@@ -12,7 +15,11 @@
 export default {
     methods: {
         onSubmit() {
-            console.log('Label value: ', this.label);
+            if (this.label === "") {
+                return;
+            }
+            this.$emit("todo-added", this.label);
+            this.label = "";
         }
     },
     data() {
